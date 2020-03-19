e4bf923d2e173c4375fa9b9545836e9a71f5908e
zzchfut <zzchao2006@126.com>
2020-03-19 22:42:16 +0800

    init demo01

---
+++ springMVCDemo01/src/com/spring/mvc/RegisterController.java
@@ -0,0 +1,16 @@
+package com.spring.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class RegisterController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+		return new ModelAndView("/WEB-INF/jsp/register.jsp");
+	}
+
+}
